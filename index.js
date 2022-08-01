11fef184206cd5b3ac737b3e3832b42372c04a13
balu <balu1072000@gmail.com>
2022-08-01 11:50:19 +0530

    added some other methods

--- index.js
+++ index.js
@@ -10,5 +10,29 @@ app.post("/create",async(req,res)=>{
     let result = await data.save()
     console.log(result)
 })
+
+app.get("/list",async(req,res)=>{
+    let data = await details.find();
+    res.send(data);
+})
+
+app.delete("/delete/:_id",async(req,res)=>{
+    console.log(req.params)
+    let data = await details.deleteOne(req.params)
+    console.log(data)
+    res.send(data)
+})
+
+app.put("/update/:_id",async(req,res)=>{
+    console.log(req.params)
+    let data=await details.updateOne(
+        req.params,
+        {$set: req.body}
+    )
+    res.send(data);
+    console.log('updated successfully!')
+})
+
+
 app.listen(5000);
 console.log('server is running on 5000')
